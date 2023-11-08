3b970a10e79e5706665b048d2f0fb556880047a9
hallan-n <hallanneves01@gmail.com>
2023-11-07 23:24:07 -0300

    Add method for create db

--- app/database/persistence.py
+++ app/database/persistence.py
@@ -6,12 +6,34 @@ from app.models.login import LoginSQL
 from app.models.product import ProductSQL
 from app.models.user import UserSQL
 
+import mysql.connector
+from mysql.connector.errors import DatabaseError
+from dotenv import load_dotenv
+import os
 
 session = get_session()
 
 
 def create_tables():
+    _create_db_mysql()
     Base.metadata.create_all(session.bind)
+
+
+def _create_db_mysql():
+    load_dotenv()
+    conn = {
+        "host": os.getenv("DB_HOST"),
+        "user": os.getenv("DB_USER"),
+        "password": os.getenv("DB_PASSWORD"),
+    }
+
+    try:
+        mydb = mysql.connector.connect(**conn)
+        mycursor = mydb.cursor()
+        mycursor.execute(f"CREATE DATABASE {os.getenv('DB_DATABASE')};")
+        print("O banco foi criado")
+    except DatabaseError as e:
+        print(">>>>>>", e.msg)
 
 
 # def create(value: BaseModel):
